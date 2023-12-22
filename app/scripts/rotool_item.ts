a2207d3bad207d242138c1a59f3a26848045d640
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2023-12-22 15:31:20 +0900

    [modify] Update v0.0.4

--- app/scripts/rotool_item.ts
+++ app/scripts/rotool_item.ts
@@ -1,13 +1,13 @@
-import { load } from 'cheerio'
-
 const main = async () => {
-    const html = load(document.body.innerHTML)
-    const item_name = html("body > main > article > h1").text().trim()
+    const regex_item_id = /^\/item\/(\d+)\/?/
+    const matches = location.pathname.match(regex_item_id)
 
-    if (item_name == "アイテム検索")
+    if (matches == undefined || matches?.length == 0)
         return
 
-    fetch('https://rodb.aws.0nyx.net/bokehro-check?name=' + encodeURI(item_name), {
+    const item_id = Number(matches[1])
+
+    fetch('https://rodb.aws.0nyx.net/bokehro-check/' + item_id, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -16,14 +16,14 @@ const main = async () => {
         .then((response) => response.json())
         .then((result) => {
             if (result.success == true) {
-                element_injection(result.request.item_name, result.item_id || -1, result.export_img_url)
+                element_injection(item_id, result.data.item_name, result.export_img_url)
             }
         })
         .catch(error => console.log(error))
 }
 
-const element_injection = (item_name: string, item_id: number, export_img_url: string) => {
-    const item_name_eocodeed: string = encodeURI(item_name)
+const element_injection = (item_id: number, item_name: string, export_img_url: string) => {
+    const item_name_encodeed: string = encodeURI(item_name)
     const element_section = document.createElement("section")
     element_section.setAttribute("class", "content-wrap transction-wrap")
     element_section.innerHTML = `
@@ -49,7 +49,7 @@ hr.line {
 <h3 class="section-ttl_mini">Extension Links</h3>
 
 <div class="center">
-<a href="https://rodb.aws.0nyx.net/bokehro?name=`+ item_name_eocodeed + `#plot" target="_blank">
+<a href="https://rodb.aws.0nyx.net/bokehro/`+ item_id + `#plot" target="_blank">
 <img src="`+ export_img_url + `" alt="RODB: Bokeh RO png image" class="bokehro" /><br/>
 RODB - Bokeh RO : `+ item_name + ` を開く</a><br/>
 </div>
@@ -57,7 +57,7 @@ RODB - Bokeh RO : `+ item_name + ` を開く</a><br/>
 <hr class="line">
 
 <div class="center">
-<a href="https://roitemsearch.web.app/?search=`+ item_name_eocodeed + `" target="_blank">
+<a href="https://roitemsearch.web.app/?search=`+ item_name_encodeed + `" target="_blank">
 ROアイテム検索くん で開く</a><br/>
 </div>
 
